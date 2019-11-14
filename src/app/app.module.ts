055ae05aa97ed82cfe78e19bfa1ad9a37e246768
Quang Nguyen <quangnd.92@gmail.com>
2019-11-14 17:08:01 +0700

    update code

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    InfiniteScrollModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
